9f56c0ea7a067ed6d243b667a92c63b6cc60aad6
alielgabri <ali.elgabri@gmail.com>
2019-01-15 20:11:35 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/Cs4500Sp19sAhgApplication.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4500Sp19sAhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4500Sp19sAhgApplication.class, args);
+	}
+
+}
+
