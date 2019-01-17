6128328c0574c73848d7cded1a05ce6da585bff8
texzone <ali.elgabri@gmail.com>
2019-01-16 21:48:10 -0500

    submit assignment

---
+++ cs4500-sp19s1-alielgabri/src/main/java/com/example/cs4500sp19s1jga/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.cs4500sp19s1jga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
